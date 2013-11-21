32744517b5bba7dffaba5aa090c2125e93ea2861
Shane Girish <shaneGirish@gmail.com>
2013-11-21 16:48:55 +0000

    Core changes

--- src/LargeImageViewer.java
+++ src/LargeImageViewer.java
@@ -1,5 +1,9 @@
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -7,109 +11,177 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+
 @SuppressWarnings("serial")
 public class LargeImageViewer extends JFrame {
-  protected JComponent component;
-  protected int rows, cols, tileSize;
-  protected BufferedImage tiles[][];
-  
-  public LargeImageViewer() {
-    component = new InnerComponent();
-    component.setSize(800, 600);
+	protected JComponent component;
 
-    this.setSize(800, 600);
-    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-    this.setVisible(true);
-  }
+	public LargeImageViewer(String url) throws IOException {
+		long t1, t2;
 
-  public static void main(String[] args) {
-    LargeImageViewer viewer = new LargeImageViewer();
-    try {
-      viewer.loadImage("Edinburgh.jpg");
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
+		t1 = System.currentTimeMillis();
+		BufferedImage image = ImageIO.read(new File(url));
+		t2 = System.currentTimeMillis();
+		System.out.println("Image loaded in " + (t2 - t1) + " milliseconds.");
 
-  private void loadImage(String url) throws IOException {
-    long t1, t2;
-    tileSize = 100;
-    File file = new File(url);
+		component = new InnerComponent(image);
+		this.add(component);
+		this.setSize(800, 600);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
 
-    System.out.println("Loading file: " + url + " (" + (file.exists()?"exists":"missing") + ")");
-    t1 = System.currentTimeMillis();
-    BufferedImage image = ImageIO.read(file);
-    int old_w = image.getWidth();
-    int old_h = image.getHeight();
-    int w = old_w + tileSize - old_w%tileSize;
-    int h = old_w + tileSize - old_w%tileSize;
-    BufferedImage expandedImage = new BufferedImage(w, h, image.getType());
-    Graphics2D graphics = (Graphics2D) expandedImage.getGraphics();
-    graphics.drawImage(image, 0, 0, old_w, old_h, null);
-    graphics.dispose();
-    image.flush();
-    image = null;
-    t2 = System.currentTimeMillis();
-    System.out.println("Image loaded in " + (t2 - t1) + " milliseconds.");
-    t1 = System.currentTimeMillis();
-    rows = h/tileSize;
-    cols = w/tileSize;
+	public static void main(String[] args) {
+		try {
+			new LargeImageViewer("Edinburgh.jpg");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
 
+@SuppressWarnings("serial")
+class InnerComponent extends JComponent implements MouseListener, MouseMotionListener {
+	protected BufferedImage tiles[][];
 
-    tiles = new BufferedImage[rows][cols];
-    for (int x = 0; x < rows; x++) {
-      for (int y = 0; y < cols; y++) {
-        tiles[x][y] = expandedImage.getSubimage(tileSize * y, tileSize * x, tileSize, tileSize);
-      }  
-    }
-    t2 = System.currentTimeMillis();
-    System.out.println("Image split in " + (t2 - t1) + " milliseconds.");
-    t1 = System.currentTimeMillis();
-    //    for (int x = 0; x < rows; x++) {
-    //      for (int y = 0; y < cols; y++) {
-    //        ImageIO.write(tiles[x][y], "jpg", new File("/home/zacfer/Pictures/img " + x + "," + y + ".jpg"));
-    //      }  
-    //    }
-    this.add(component);
-    component.repaint();
+	protected final int tileSize;
 
-    t2 = System.currentTimeMillis();
-    System.out.println("Image saved in " + (t2 - t1) + " milliseconds.");
-  }
+	protected int width, height, rows, cols;
 
+	protected Point anchor, dragStart;
 
+	protected double scale = 1;
 
-private class InnerComponent extends JComponent {
-	protected int offsetX = 0;
-	protected int offestY = 0;
+	public InnerComponent(BufferedImage image) {
+		this(image, 100);
+	}
+
+	public InnerComponent(BufferedImage image, int tileSize) {
+		if (tileSize < 50) {
+			throw new IllegalArgumentException("Specified tile size is not valid.");
+		} else {
+			this.tileSize = tileSize;
+		}
+		calculateTiles(padImage(image));
+		addMouseListener(this);
+		addMouseMotionListener(this);
+
+	}
+
+	protected BufferedImage padImage(BufferedImage image) {
+		int old_width = image.getWidth();
+		int old_height = image.getHeight();
+
+		width = old_width + tileSize - old_width % tileSize;
+		height = old_height + tileSize - old_height % tileSize;
+		
+		anchor = new Point(width/2, height/2);
+
+		BufferedImage paddedImage = new BufferedImage(width, height, image.getType());
+		Graphics2D graphics = (Graphics2D) paddedImage.getGraphics();
+		graphics.drawImage(image, 0, 0, old_width, old_height, null);
+		graphics.dispose();
+
+		image.flush();
+		image = null;
+
+		return paddedImage;
+	}
+
+	protected void calculateTiles(BufferedImage image) {
+		rows = height / tileSize;
+		cols = width / tileSize;
+		tiles = new BufferedImage[cols][rows];
+
+		for (int x = 0; x < cols; x++) {
+			for (int y = 0; y < rows; y++) {
+				tiles[x][y] = image.getSubimage(tileSize * x, tileSize * y, tileSize, tileSize);
+			}
+		}
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		
+		int screen_width = this.getWidth();
+		int screen_height = this.getHeight();
+				
+		int rows_on_screen = (int) Math.ceil(screen_width / (tileSize * scale)) + 1;
+		int cols_on_screen = (int) Math.ceil(screen_height / (tileSize * scale)) + 1;
+		
+		int first_tile_index_x = (int) Math.floor((anchor.x - (screen_width * scale)/2)/tileSize);
+		int first_tile_index_y = (int) Math.floor((anchor.y - (screen_height * scale)/2)/tileSize);
+		
+		int last_tile_index_x = first_tile_index_x + cols_on_screen;
+		int last_tile_index_y = first_tile_index_y + rows_on_screen;
+		
+		int max_anchor_x = width - screen_width/2;
+		int max_anchor_y = height - screen_height/2;
+		int min_anchor_x = width - screen_width/2;
+		int min_anchor_y = height - screen_height/2;
+		
+		if(anchor.x > max_anchor_x) {
+			first_tile_index_x -= last_tile_index_x - rows + 1;
+			last_tile_index_x = rows - 1;
+			anchor.x = max_anchor_x;
+		}
+		
+		if(anchor.y > max_anchor_y) {
+			first_tile_index_y -= last_tile_index_y - cols + 1;
+			last_tile_index_y = cols - 1;
+			anchor.y = max_anchor_y;
+		}
+		
+		if(first_tile_index_x < 0) {
+			last_tile_index_x -= first_tile_index_x;
+			first_tile_index_x = 0;
+			anchor.x = screen_width/2;
+		}
+		
+		if(first_tile_index_y < 0) {
+			last_tile_index_y -= first_tile_index_y;
+			first_tile_index_y = 0;
+			anchor.y = screen_height/2;
+		}
+		
+		int x, y, x_cood, y_cood, tile_pos_x, tile_pos_y;
+		
+		for (x = 0 ; x < rows_on_screen; x++) {
+			for (y = 0; y < cols_on_screen; y++) {
+				tile_pos_x = first_tile_index_x + x;
+				tile_pos_y = first_tile_index_y + y;
+				
+				if(tile_pos_x >= cols || tile_pos_y >= rows) {
+					break;
+				}
+				
+				x_cood = tile_pos_x * tileSize - anchor.x + screen_width/2;
+				y_cood = tile_pos_y * tileSize - anchor.y + screen_height/2;
+				
+				g2d.drawImage(tiles[tile_pos_x][tile_pos_y], x_cood, y_cood, null);
+			}
+		}
+		super.paintComponent(g);
+	}
+
+    @Override public void mouseMoved(MouseEvent e) {}
+    @Override public void mouseExited(MouseEvent event) {}
+	@Override public void mouseClicked(MouseEvent event) {}
+    @Override public void mouseEntered(MouseEvent event) {}
+    @Override public void mouseReleased(MouseEvent event) {}
+    
     @Override
-    public void paintComponent(Graphics g) { 
-      Graphics2D g2d = (Graphics2D) g;
-      
-      
-      
-      
-      
-      
-      int centerRow = rows/2, 
-          centerCol = cols/2, 
-          tileCols = this.getWidth() / tileSize, 
-          tileRows = this.getHeight() / tileSize,
-          centerTileRow = tileRows / 2,
-          centerTileCol = tileCols / 2,
-          tileCenter = tileSize / 2;
-      
-      int tmpX, tmpY;
-      System.out.println(centerRow + " " + centerCol + " " + tileCols + " " + tileRows);
-      for (int x = 0; x < tileRows; x++) {
-        for (int y = 0; y < tileCols; y++) {
-          tmpY = centerCol - centerTileCol + y;
-          tmpX = centerRow - centerTileRow + x;
-          g2d.drawImage(tiles[tmpX][tmpY], tileSize * y, tileSize * x, null);
-        }
-      }
-      super.paintComponent(g);
-    }
-  }
+    public void mousePressed(MouseEvent event) {
+    	dragStart = event.getPoint();
+    }   
 
+    @Override
+    public void mouseDragged(MouseEvent event) {
+    	Point dragEnd = event.getPoint();
+    	anchor.x -= dragEnd.x - dragStart.x;
+    	anchor.y -= dragEnd.y - dragStart.y;
+    	dragStart = dragEnd;
+    	repaint();
+    }
 }
