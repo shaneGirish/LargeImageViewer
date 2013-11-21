d8b93e09b4e579e2fc643f836b68e1f778d052dd
Shane Girish <shaneGirish@gmail.com>
2013-11-21 19:44:20 +0000

    MouseWheelListener and Drag Limits

--- src/LargeImageViewer.java
+++ src/LargeImageViewer.java
@@ -4,6 +4,8 @@ import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -17,12 +19,7 @@ public class LargeImageViewer extends JFrame {
 	protected JComponent component;
 
 	public LargeImageViewer(String url) throws IOException {
-		long t1, t2;
-
-		t1 = System.currentTimeMillis();
 		BufferedImage image = ImageIO.read(new File(url));
-		t2 = System.currentTimeMillis();
-		System.out.println("Image loaded in " + (t2 - t1) + " milliseconds.");
 
 		component = new InnerComponent(image);
 		this.add(component);
@@ -41,7 +38,7 @@ public class LargeImageViewer extends JFrame {
 }
 
 @SuppressWarnings("serial")
-class InnerComponent extends JComponent implements MouseListener, MouseMotionListener {
+class InnerComponent extends JComponent implements MouseListener, MouseMotionListener, MouseWheelListener {
 	protected BufferedImage tiles[][];
 
 	protected final int tileSize;
@@ -65,7 +62,7 @@ class InnerComponent extends JComponent implements MouseListener, MouseMotionLis
 		calculateTiles(padImage(image));
 		addMouseListener(this);
 		addMouseMotionListener(this);
-
+		addMouseWheelListener(this);
 	}
 
 	protected BufferedImage padImage(BufferedImage image) {
@@ -74,8 +71,8 @@ class InnerComponent extends JComponent implements MouseListener, MouseMotionLis
 
 		width = old_width + tileSize - old_width % tileSize;
 		height = old_height + tileSize - old_height % tileSize;
-		
-		anchor = new Point(width/2, height/2);
+
+		anchor = new Point(width / 2, height / 2);
 
 		BufferedImage paddedImage = new BufferedImage(width, height, image.getType());
 		Graphics2D graphics = (Graphics2D) paddedImage.getGraphics();
@@ -100,88 +97,82 @@ class InnerComponent extends JComponent implements MouseListener, MouseMotionLis
 		}
 	}
 
+	protected void checkAnchor(int screen_width, int screen_height) {
+		int max_anchor_x = width - screen_width / 2;
+		int max_anchor_y = height - screen_height / 2;
+		int min_anchor_x = screen_width / 2;
+		int min_anchor_y = screen_height / 2;
+
+		if (anchor.x > max_anchor_x) {
+			anchor.x = max_anchor_x;
+		} else if (anchor.x < min_anchor_x) {
+			anchor.x = min_anchor_x;
+		}
+
+		if (anchor.y > max_anchor_y) {
+			anchor.y = max_anchor_y;
+		} else if (anchor.y < min_anchor_y) {
+			anchor.y = min_anchor_y;
+		}
+	}
+
 	@Override
 	public void paintComponent(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
-		
+
 		int screen_width = this.getWidth();
 		int screen_height = this.getHeight();
-				
+
+		checkAnchor(screen_width, screen_height);
+
 		int rows_on_screen = (int) Math.ceil(screen_width / (tileSize * scale)) + 1;
 		int cols_on_screen = (int) Math.ceil(screen_height / (tileSize * scale)) + 1;
-		
-		int first_tile_index_x = (int) Math.floor((anchor.x - (screen_width * scale)/2)/tileSize);
-		int first_tile_index_y = (int) Math.floor((anchor.y - (screen_height * scale)/2)/tileSize);
-		
-		int last_tile_index_x = first_tile_index_x + cols_on_screen;
-		int last_tile_index_y = first_tile_index_y + rows_on_screen;
-		
-		int max_anchor_x = width - screen_width/2;
-		int max_anchor_y = height - screen_height/2;
-		int min_anchor_x = width - screen_width/2;
-		int min_anchor_y = height - screen_height/2;
-		
-		if(anchor.x > max_anchor_x) {
-			first_tile_index_x -= last_tile_index_x - rows + 1;
-			last_tile_index_x = rows - 1;
-			anchor.x = max_anchor_x;
-		}
-		
-		if(anchor.y > max_anchor_y) {
-			first_tile_index_y -= last_tile_index_y - cols + 1;
-			last_tile_index_y = cols - 1;
-			anchor.y = max_anchor_y;
-		}
-		
-		if(first_tile_index_x < 0) {
-			last_tile_index_x -= first_tile_index_x;
-			first_tile_index_x = 0;
-			anchor.x = screen_width/2;
-		}
-		
-		if(first_tile_index_y < 0) {
-			last_tile_index_y -= first_tile_index_y;
-			first_tile_index_y = 0;
-			anchor.y = screen_height/2;
-		}
-		
-		int x, y, x_cood, y_cood, tile_pos_x, tile_pos_y;
-		
-		for (x = 0 ; x < rows_on_screen; x++) {
-			for (y = 0; y < cols_on_screen; y++) {
-				tile_pos_x = first_tile_index_x + x;
-				tile_pos_y = first_tile_index_y + y;
-				
-				if(tile_pos_x >= cols || tile_pos_y >= rows) {
-					break;
+
+		int first_tile_index_x = (int) Math.floor((anchor.x - (screen_width * scale) / 2) / tileSize);
+		int first_tile_index_y = (int) Math.floor((anchor.y - (screen_height * scale) / 2) / tileSize);
+
+		int last_tile_index_x = first_tile_index_x + rows_on_screen;
+		int last_tile_index_y = first_tile_index_y + cols_on_screen;
+
+		int x, y, x_cood, y_cood;
+
+		for (x = first_tile_index_x; x < last_tile_index_x; x++) {
+			for (y = first_tile_index_y; y < last_tile_index_y; y++) {
+				if (x >= cols || y >= rows || x < 0 || y < 0) {
+					continue;
 				}
-				
-				x_cood = tile_pos_x * tileSize - anchor.x + screen_width/2;
-				y_cood = tile_pos_y * tileSize - anchor.y + screen_height/2;
-				
-				g2d.drawImage(tiles[tile_pos_x][tile_pos_y], x_cood, y_cood, null);
+
+				x_cood = x * tileSize - anchor.x + screen_width / 2;
+				y_cood = y * tileSize - anchor.y + screen_height / 2;
+
+				g2d.drawImage(tiles[x][y], x_cood, y_cood, null);
 			}
 		}
 		super.paintComponent(g);
 	}
 
-    @Override public void mouseMoved(MouseEvent e) {}
-    @Override public void mouseExited(MouseEvent event) {}
+	@Override public void mouseMoved(MouseEvent event) {}
+	@Override public void mouseExited(MouseEvent event) {}
 	@Override public void mouseClicked(MouseEvent event) {}
-    @Override public void mouseEntered(MouseEvent event) {}
-    @Override public void mouseReleased(MouseEvent event) {}
-    
-    @Override
-    public void mousePressed(MouseEvent event) {
-    	dragStart = event.getPoint();
-    }   
+	@Override public void mouseEntered(MouseEvent event) {}
+	@Override public void mouseReleased(MouseEvent event) {}
 
-    @Override
-    public void mouseDragged(MouseEvent event) {
-    	Point dragEnd = event.getPoint();
-    	anchor.x -= dragEnd.x - dragStart.x;
-    	anchor.y -= dragEnd.y - dragStart.y;
-    	dragStart = dragEnd;
-    	repaint();
-    }
+	@Override
+	public void mousePressed(MouseEvent event) {
+		dragStart = event.getPoint();
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent event) {
+		Point dragEnd = event.getPoint();
+		anchor.x -= dragEnd.x - dragStart.x;
+		anchor.y -= dragEnd.y - dragStart.y;
+		dragStart = dragEnd;
+		repaint();
+	}
+
+	@Override
+	public void mouseWheelMoved(MouseWheelEvent event) {
+		int steps = event.getWheelRotation();
+	}
 }
