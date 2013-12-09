aae17e51db81d172a1c8a260efcb1014138cb5b4
Shane Girish <shaneGirish@gmail.com>
2013-12-09 09:43:26 +0000

    Fix Double key issues
    Remove thread functionality

--- src/Tile.java
+++ src/Tile.java
@@ -1,20 +1,29 @@
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.imgscalr.AsyncScalr;
+import org.imgscalr.Scalr;
 import org.imgscalr.Scalr.Method;
 
 public class Tile {
-	final int width, height;
-	final BufferedImage data;
-	final ConcurrentHashMap<Integer, BufferedImage> cache = new ConcurrentHashMap<Integer, BufferedImage>();
-	final ConcurrentHashMap<Integer, Integer> cacheHitRatio = new ConcurrentHashMap<Integer, Integer>();
+	protected final Tile self;
+	protected final int x, y;
+	protected final int width, height;
+	protected final BufferedImage data;
+	protected final ConcurrentHashMap<Integer, BufferedImage> cache = new ConcurrentHashMap<Integer, BufferedImage>();
+	protected final ConcurrentHashMap<Integer, Integer> reqCounts = new ConcurrentHashMap<Integer, Integer>();
+
+	protected final ArrayList<TileResizeListener> listeners = new ArrayList<TileResizeListener>();
 	
-	public Tile(BufferedImage data) {
+	public Tile(int x, int y, BufferedImage data) {
+		this.self = this;
+		this.x = x;
+		this.y = y;
 		this.width = data.getWidth();
 		this.height = data.getHeight();
 		this.data = data;
-		cache.put(1, this.data);
+		cache.put(getKey(1.0), this.data);
 	}
 	
 	private Integer getKey(double scale) {
@@ -24,21 +33,31 @@ public class Tile {
 	public BufferedImage getScaledTile(final double scale) {
 		final Integer key = getKey(scale);
 		
-		if(!cache.containsKey(scale)) {
+		reqCounts.putIfAbsent(key, 1);
+		reqCounts.put(key, reqCounts.get(key) + 1);
+		
+		if(!cache.containsKey(key)) {
 			int newWidth = (int) (width * scale);
 			int newHeight = (int) (height * scale);
-			TileResizer.add(
+			/*TileResizer.add(
 				AsyncScalr.resize(data, Method.ULTRA_QUALITY, newWidth, newHeight),
 				new Callback<BufferedImage>() {
 					@Override void invoke(BufferedImage result) {
 						cache.put(key, result);
-						cacheHitRatio.put(key, 1);
+
+						for (TileResizeListener listener : listeners) {
+							listener.tileResized(self, scale);
+						}
 					}
 				}
 			);
-		} else {
-			cacheHitRatio.put(key, cacheHitRatio.get(key) + 1);
+			return Scalr.resize(data, Method.SPEED, newWidth, newHeight);
+			*/
+			BufferedImage result = Scalr.resize(data, Method.QUALITY, newWidth, newHeight);
+			cache.put(key, result);
+			return result;
 		}
+			
 		return cache.get(key);
 	}
 }
